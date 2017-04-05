f6585d4e818e85adad8b02b4edef2cc820509288
kartik-pawar <kartikpawar2012@gmail.com>
2017-04-05 11:03:55 +0530

    update1

--- predictor.js
+++ predictor.js
@@ -1,6 +1,21 @@
-var canvas=document.getElementsByTagName("canvas");
-console.log(canvas);
-var canvasctx=canvas[0].getContext("2d");
 function predict(){
+	var canvas=document.getElementsByTagName("canvas");
+	console.log(canvas);
+	var canvasctx=canvas[0].getContext("2d");
 	console.log(canvasctx);
+
+	var cheight=canvas[0].clientHeight;
+	var cwidth=canvas[0].clientHeight;
+	var imgd=canvasctx.getImageData(0,0,cheight,cwidth);
+	console.log(imgd);
+	
+	var pix=imgd.data;
+	console.log(pix);
+	var pixels=new Array();
+	for(i=0;i<cheight;i+=10)
+		for(j=0	;j<cwidth*4;j+=40){
+			pixels.push(pix[i+j]);
+			console.log(i+j);
+		}
+	console.log(pixels);
 }
