eca511a399e2eab8fa95ea7e62a1fcdd4918fa5f
kartik-pawar <kartikpawar2012@gmail.com>
2017-05-08 23:18:48 +0530

    Working model
    Does not consider that image needs to be 20x20 centered in 28x28 image. Need to resolve that.

--- predictor.js
+++ predictor.js
@@ -1,14 +1,33 @@
 function predict(){
-	reqPixels=scaling();
+	var reqPixels=scaling();
+	var intercept=loadIntercept();
+	var coef=loadCoef();
+	var predictions=new Array();
+	//console.log(reqPixels);
+	for(i=0;i<intercept.length;i++){
+		predictions.push(intercept[i]);
+		for(j=0;j<coef[i].length;j++){
+			predictions[i]+=reqPixels[j]*coef[i][j];
+			//console.log(i+':  '+reqPixels[j]+'*'+coef[i][j]+'= '+reqPixels[i]*coef[i][j]);
+		}
+		console.log(predictions[i]);
+	}
 
+	maxpred=0;
+	for (var i = predictions.length - 1; i >= 0; i--) {
+		if(predictions[i]>predictions[maxpred]) maxpred=i;
+	}
+	console.log(maxpred);
+	document.getElementById('prediction').value=maxpred;
 }
 
 function scaling(){
 	var canvas=document.getElementsByTagName("canvas");
-	var canvasContext=canvas[1].getContext("2d");
+	var canvasContext=canvas[0].getContext("2d");
 
-	var cheight=canvas[1].clientHeight;
-	var cwidth=canvas[1].clientHeight;
+	var cheight=canvas[0].clientHeight;
+	var cwidth=canvas[0].clientHeight;
+	console.log(cheight+" "+cwidth);
 	var drawingImageData=canvasContext.getImageData(0,0,cheight,cwidth);
 	
 	var drawingImageDataPixels=drawingImageData.data;
@@ -24,8 +43,7 @@ function scaling(){
 			//console.log(current+" "+drawingImageDataPixels[current]);
 		}
 	}
-	return reqPixels;
-	/*	
+	/*
 	var checkCanvas = document.getElementById("showimg");
 	var checkCanvasContext = checkCanvas.getContext("2d");
 	var checkImageData = checkCanvasContext.getImageData(0,0,28,28);
@@ -38,5 +56,31 @@ function scaling(){
 	}
 	console.log(checkImageData.data);
 	checkCanvasContext.putImageData(checkImageData, 0, 0);
+	
+	var egimg=reqPixels;
+	console.log(egimg);
+	var checkCanvas = document.getElementById("showimg");
+	var checkCanvasContext = checkCanvas.getContext("2d");
+	var checkImageData = checkCanvasContext.getImageData(0,0,280,280);
+	//console.log(checkImageData);
+
+	var arr=[]
+	for(i=0;i<28;i++){
+		for(x=0;x<10;x++){
+			for(j=0;j<28;j++){
+				for(y=0;y<10;y++){
+					checkImageData.data[y*4+j*40+x*280*4+i*2800*4] = checkImageData.data[1+y*4+j*40+x*280*4+i*2800*4] = checkImageData.data[2+y*4+j*40+x*280*4+i*2800*4] = egimg[(i*28+j)];
+					checkImageData.data[3+y*4+j*40+x*280*4+i*2800*4]=255;
+					arr.push((y*4+j*40+x*280*4+i*2800*4)/4+" index: "+(i*28+j)+" val:"+egimg[(i*28+j)]);
+
+				}
+			}
+		}
+	}
+	console.log(arr);
+
+	checkCanvasContext.putImageData(checkImageData, 0, 0);
 	*/
+	return reqPixels;
+	
 }
